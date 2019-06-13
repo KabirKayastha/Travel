557b4620bacc7395e18a728658258a5e1abf20b6
KabirKayastha <kayas.kabie@gmail.com>
2019-06-13 10:36:39 +0545

    getting started

---
+++ js/owlCarousel/o-carousel.js
@@ -0,0 +1,23 @@
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin: 20,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            },
+            1050:{
+                items:5
+            }
+        }
+    })
+
