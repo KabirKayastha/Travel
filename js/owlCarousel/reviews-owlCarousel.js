553735792ac9d1da69c870b1d480cf196f7117a4
KabirKayastha <kayas.kabie@gmail.com>
2019-07-03 16:26:19 +0545

    landing page completed

---
+++ js/owlCarousel/reviews-owlCarousel.js
@@ -0,0 +1,29 @@
+//owl carousel 
+$('#reviews .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    lazyLoad: true,
+    dots:false,
+    
+    autoplay: true,
+    autoplayTimeout: 10000,
+    autoplayHoverPause: true,
+    smartSpeed: 2500,
+    dragEndSpeed: 700,
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        },
+        1050:{
+            items:2
+        }
+    }
+});
