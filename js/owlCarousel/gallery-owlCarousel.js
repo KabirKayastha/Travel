deafcda953803ff4c6e447a32f053a37309e43a5
Kabir Kayastha <kayas.kabie@gmail.com>
2019-11-14 13:19:38 +0545

    gallery-caption added

--- js/owlCarousel/gallery-owlCarousel.js
+++ js/owlCarousel/gallery-owlCarousel.js
@@ -1,29 +1,29 @@
-//owl carousel 
-$('#gallery .owl-carousel').owlCarousel({
-    loop: true,
-    margin: 20,
-    nav: true,
-    dotsEach: true,
-    lazyLoad: true,
+//owl carousel
+$("#gallery .owl-carousel").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  dotsEach: true,
+  lazyLoad: true,
 
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    smartSpeed: 1000,
-    dragEndSpeed: 700,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  smartSpeed: 1000,
+  dragEndSpeed: 700,
 
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        },
-        1050:{
-            items:4
-        }
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    },
+    1050: {
+      items: 4
     }
+  }
 });
